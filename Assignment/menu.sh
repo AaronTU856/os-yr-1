00d7a6fdb59a9a58f3ea3f716a6999ec9b51e0dd
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2023-03-23 09:24:10 +0000

    Assignment menu changes

---
+++ Assignment/menu.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+while true;
+ do
+    clear
+    echo "* Bash Commands *"
+
+    echo "1. List files"
+
+    echo "2. Show free disk space"
+
+
+
+    echo "3. Show system path"
+
+    echo "4. Display command history"
+
+    echo "5. Backup files"
+
+    echo "6. Exit"
+
+    read -p "Enter your choice from the above options: " choice
+
+    case $choice in
+        "1") 
+            ls 
+            ;;
+
+        "2") 
+            df -h 
+            ;;
+
+        "3") 
+            echo 
+            $PATH 
+            ;;
+
+        "4") 
+            history 
+            ;;
+
+        "5")
+            read -p "Enter directory to backup: " dir
+            mkdir BackupFolder
+            cp -R $dir BackupFolder
+            ls BackupFolder
+            ;;
+
+        "6") 
+            echo "Goodbye"
+            exit 
+            ;;
+
+        *) echo "Invalid choice. Press enter to try again."
+           read ;;
+    esac
+done
